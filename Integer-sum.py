683fb2413057cb4aac9f9ab25ff746c7b3693f44
Idvaen <75420852+Idvaen@users.noreply.github.com>
2021-10-05 19:53:21 +0200

    Update Integer-sum.py

--- Integer-sum.py
+++ Integer-sum.py
@@ -1,11 +1,12 @@
-def integer_sum(input):
-    n = int(input)
+def integer_sum(n):
+    n = int(n)
+    limit = 10**4
     sum = 0
 
-    if n > 0 and n < 10**4:
+    if n > 0 and n < limit:
         for i in range(n+1):
             sum += i
-    elif n < 0 and n > -10**4:
+    elif n < 0 and n > -limit:
         sum = 1
         for i in range(-n+1):
             sum -= i
